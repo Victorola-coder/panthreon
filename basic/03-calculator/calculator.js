4d1a879f1d895d72214166880df561f7e8ca89b8
Victor Ola <victoluolatunji@gmail.com>
2024-01-10 22:33:08 +0100

    [chore] wrote the code with if else

--- basic/03-calculator/calculator.js
+++ basic/03-calculator/calculator.js
@@ -1,25 +1,46 @@
+// switch case approach.
+
+// function calculator(num1, num2, operator) {
+//   let result;
+
+//   switch (operator) {
+//     case '+':
+//       result = num1 + num2;
+//       break;
+//     case '-':
+//       result = num1 - num2;
+//       break;
+
+//     case '/':
+//       result = num1 / num2;
+//       break;
+//     case '*':
+//       result = num1 * num2;
+//       break;
+
+//     default:
+//       throw new Error('Invalid Operators');
+//   }
+//   return result;
+// }
+
+// conditional statements "if" and "else" approach
+
 function calculator(num1, num2, operator) {
   let result;
 
-  switch (operator) {
-    case '+':
-      result = num1 + num2;
-      break;
-    case '-':
-      result = num1 - num2;
-      break;
-
-    case '/':
-      result = num1 / num2;
-      break;
-    case '*':
-      result = num1 * num2;
-      break;
-
-    default:
-      throw new Error('Invalid Operators');
+  if (operator === '+') {
+    result = num1 + num2;
+  } else if (operator === '-') {
+    result = num1 - num2;
+  } else if (operator === '/') {
+    result = num1 / num2;
+  } else if (operator === '*') {
+    result = num1 * num2;
+  } else {
+    throw new Error('Invalid operator');
   }
+
   return result;
 }
-
 module.exports = calculator;
--- basic/03-calculator/calculator.test.js
+++ basic/03-calculator/calculator.test.js
@@ -1,6 +1,20 @@
 const calculator = require('./calculator');
 
 test('should calculate the arithmetic of two inputs data correctly', () => {
-  const result = calculator(2, 4, '*');
-  expect(result).toBe(8);
+  const num1 = 7;
+  const num2 = 2;
+
+  //   addition fx
+  expect(calculator(num1, num2, '+')).toBe(9);
+
+  // subtraction fx
+  expect(calculator(num1, num2, '-')).toBe(5);
+
+  // multiplication fx
+
+  expect(calculator(num1, num2, '*')).toBe(14);
+
+  //   division fx
+
+  expect(calculator(num1, num2, '/')).toBeCloseTo(3.5);
 });
