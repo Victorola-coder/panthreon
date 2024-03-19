e8f9e1ebe741c6a58b5cd97576120ab951d006e1
Victor Olatunji <victoluolatunji@gmail.com>
2024-03-19 02:21:42 +0100

    tbc

---
+++ hackerank/simpleArraySum/simpleArraySum.js
@@ -0,0 +1,58 @@
+"use strict";
+
+const fs = require("fs");
+
+process.stdin.resume();
+process.stdin.setEncoding("utf-8");
+
+let inputString = "";
+let currentLine = 0;
+
+process.stdin.on("data", function (inputStdin) {
+  inputString += inputStdin;
+});
+
+process.stdin.on("end", function () {
+  inputString = inputString.split("\n");
+
+  main();
+});
+
+function readLine() {
+  return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'simpleArraySum' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts INTEGER_ARRAY ar as parameter.
+ */
+
+function simpleArraySum(ar) {
+  // Write your code here
+  let total = 0;
+
+  for (const num of ar) {
+    total += num;
+  }
+
+  return total;
+}
+
+function main() {
+  const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+  const arCount = parseInt(readLine().trim(), 10);
+
+  const ar = readLine()
+    .replace(/\s+$/g, "")
+    .split(" ")
+    .map((arTemp) => parseInt(arTemp, 10));
+
+  const result = simpleArraySum(ar);
+
+  ws.write(result + "\n");
+
+  ws.end();
+}
