4647a084e914a6a7f43be8a4136e1fdf7677ee06
Victor || VickyJay <77025670+Victorola-coder@users.noreply.github.com>
2024-08-12 13:19:13 -0700

    remove unused codes

--- hackerank/simpleArraySum/simpleArraySum.js
+++ hackerank/simpleArraySum/simpleArraySum.js
@@ -1,27 +1,3 @@
-"use strict";
-
-const fs = require("fs");
-
-process.stdin.resume();
-process.stdin.setEncoding("utf-8");
-
-let inputString = "";
-let currentLine = 0;
-
-process.stdin.on("data", function (inputStdin) {
-  inputString += inputStdin;
-});
-
-process.stdin.on("end", function () {
-  inputString = inputString.split("\n");
-
-  main();
-});
-
-function readLine() {
-  return inputString[currentLine++];
-}
-
 /*
  * Complete the 'simpleArraySum' function below.
  *
@@ -38,21 +14,4 @@ function simpleArraySum(ar) {
   }
 
   return total;
-}
-
-function main() {
-  const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
-
-  const arCount = parseInt(readLine().trim(), 10);
-
-  const ar = readLine()
-    .replace(/\s+$/g, "")
-    .split(" ")
-    .map((arTemp) => parseInt(arTemp, 10));
-
-  const result = simpleArraySum(ar);
-
-  ws.write(result + "\n");
-
-  ws.end();
 }
